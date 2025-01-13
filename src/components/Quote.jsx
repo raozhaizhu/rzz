cfa94127c071662f734d0f9e938f8d63be9edac1
raozhaizhu <raozhaizhu@gmail.com>
2025-01-13 23:40:28 +0800

    go

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -5,12 +5,8 @@ import colors from "../data/colors.json";
 
 const Quote = () => {
     // 设置随机索引的状态
-    const [quoteIndex, setQuoteIndex] = useState(
-        Math.floor(Math.random() * quotes.length)
-    );
-    const [colorIndex, setColorIndex] = useState(
-        Math.floor(Math.random() * colors.length)
-    );
+    const [quoteIndex, setQuoteIndex] = useState(Math.floor(Math.random() * quotes.length));
+    const [colorIndex, setColorIndex] = useState(Math.floor(Math.random() * colors.length));
 
     // 获取当前的 quote 和 color
     const randomQuote = quotes[quoteIndex];
@@ -25,42 +21,34 @@ const Quote = () => {
 
     // 生成分享链接
     const tweetQuote = () => {
-        const text = encodeURIComponent(
-            `"${randomQuote.text}" - ${randomQuote.author}`
-        );
+        const text = encodeURIComponent(`"${randomQuote.text}" - ${randomQuote.author}`);
         return `https://twitter.com/intent/tweet?text=${text}`;
     };
 
     // 截图链接
-    const screenshotUrl = `https://i.imgur.com/mCeKyZl.jpeg`;
+    const screenshotUrl = `https://raozhaizhu.github.io/rzz/x-cover.jpg`;
 
     return (
         <div
             className="quoteWrapper w-full flex flex-col gap-[1rem] justify-center items-center min-h-screen"
-            style={{ backgroundColor: primaryColor, color: primaryColor }}>
+            style={{ backgroundColor: primaryColor, color: primaryColor }}
+        >
             {/* 动态设置 Twitter Card Meta 信息 */}
             <Helmet>
                 <title>{randomQuote.text}</title>
                 <meta name="twitter:card" content="summary_large_image" />
                 <meta name="twitter:title" content={randomQuote.text} />
-                <meta
-                    name="twitter:description"
-                    content={`"${randomQuote.text}" - ${randomQuote.author}`}
-                />
+                <meta name="twitter:description" content={`"${randomQuote.text}" - ${randomQuote.author}`} />
                 <meta name="twitter:image" content={screenshotUrl} />
             </Helmet>
 
             <div id="quote-box" className="w-[60%] bg-[#fff] p-[3rem] rounded">
                 <article>
-                    <h2
-                        id="text"
-                        className="font-[Lato] font-[500] text-[2rem] text-center mb-[2rem]">
+                    <h2 id="text" className="font-[Lato] font-[500] text-[2rem] text-center mb-[2rem]">
                         <i className="fa fa-quote-left text-[3rem] mr-[1rem]"></i>
                         {randomQuote.text}
                     </h2>
-                    <h3
-                        id="author"
-                        className="font-[Roboto] text-end text-[1.2rem] mb-[2rem]">
+                    <h3 id="author" className="font-[Roboto] text-end text-[1.2rem] mb-[2rem]">
                         - {randomQuote.author}
                     </h3>
                 </article>
@@ -73,7 +61,8 @@ const Quote = () => {
                             style={{ backgroundColor: primaryColor }}
                             role="button"
                             href={tweetQuote()}
-                            target="_blank">
+                            target="_blank"
+                        >
                             <i className="fab fa-twitter text-[1.5rem]"></i>
                         </a>
                     </div>
@@ -81,7 +70,8 @@ const Quote = () => {
                         id="new-quote"
                         className="px-[1.5rem] py-[1rem] text-[#fff] flex justify-center items-center rounded"
                         style={{ backgroundColor: primaryColor }}
-                        onClick={setNewIndex}>
+                        onClick={setNewIndex}
+                    >
                         New Quote
                     </button>
                 </div>
