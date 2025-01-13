5d29274ac29a66aab3e9b668965021892624062b
raozhaizhu <raozhaizhu@gmail.com>
2025-01-13 10:41:55 +0800

    -fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // 引入 React Router 相关组件
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Quote from "./components/Quote"; // 引入 Quote 组件
+import Quote from "./components/Quote";
 
 function App() {
     return (
-        <Router>
-            {/* 包裹整个应用 */}
+        <Router basename="/rzz">
             <div className="app flex justify-center items-center min-h-screen min-w-screen">
-                {/* 路由配置 */}
                 <Routes>
-                    <Route path="/" element={<Quote />} /> {/* 路由 "/": 显示 Quote 组件 */}
+                    <Route path="/" element={<Quote />} />
                 </Routes>
             </div>
         </Router>
