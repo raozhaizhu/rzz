4e954b8b4d5fab919a6f40d0750da3ef17da1f6d
raozhaizhu <raozhaizhu@gmail.com>
2025-01-14 00:15:02 +0800

    f

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { HelmetProvider } from "react-helmet-async";
 
 import "./App.css";
 import Home from "./components/Home";
@@ -7,16 +6,16 @@ import Quote from "./components/Quote";
 import Markdown from "./components/Markdown";
 
 function App() {
+    // 动态设置 basename
+    const basename = process.env.NODE_ENV === "production" ? "/rzz" : "";
     return (
-        <HelmetProvider>
-            <Router basename="/rzz">
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/Markdown" element={<Markdown />} />
-                    <Route path="/Quote" element={<Quote />} />
-                </Routes>
-            </Router>
-        </HelmetProvider>
+        <Router basename={basename}>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="Markdown" element={<Markdown />} />
+                <Route path="Quote" element={<Quote />} />
+            </Routes>
+        </Router>
     );
 }
 
