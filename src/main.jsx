427a49a5a8ca6a07276c34c6a42fa0ddb21ddec9
raozhaizhu <raozhaizhu@gmail.com>
2025-01-14 14:29:58 +0800

    f

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,11 @@
+// src/main.jsx
+
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 import "./index.css";
 import App from "./App.jsx";
-
-// 解析查询参数 p 并导航到对应的路由
-function handleRedirect() {
-    const params = new URLSearchParams(window.location.search);
-    const path = params.get("p");
-
-    if (path) {
-        // 确保修改后的路径包含 /rzz
-        window.history.replaceState(null, "", `/rzz${path}`);
-    }
-}
-
-// 在 React 渲染之前调用重定向逻辑
-handleRedirect();
 
 // 获取根元素并渲染 React 应用
 const root = createRoot(document.getElementById("root"));
