3add0617c1392672bdec896492d6144aae1ba288
raozhaizhu <raozhaizhu@gmail.com>
2025-01-14 14:51:23 +0800

    f

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 
@@ -10,22 +9,6 @@ import Markdown from "./components/Markdown";
 function App() {
     // 动态设置 basename
     const basename = process.env.NODE_ENV === "production" ? "/rzz" : "";
-
-    // 解析查询参数 p 并导航到对应的路由
-    useEffect(() => {
-        function handleRedirect() {
-            const params = new URLSearchParams(window.location.search);
-            const path = params.get("p");
-
-            if (path) {
-                // 使用 basename 来处理路径
-                window.history.replaceState(null, "", `${basename}${path}`);
-            }
-        }
-
-        // 调用重定向函数
-        handleRedirect();
-    }, []); // 空依赖数组，确保只执行一次
 
     return (
         <HelmetProvider>
