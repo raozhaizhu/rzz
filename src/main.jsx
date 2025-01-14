9c8ac17b0cd986a4c4ea828852238a19f9d44473
raozhaizhu <raozhaizhu@gmail.com>
2025-01-14 09:36:53 +0800

    f

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
+
+// 添加渲染完成事件
+document.dispatchEvent(new Event("render-event"));
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,33 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import Prerenderer from "@prerenderer/webpack-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-    plugins: [react()],
-    base: mode === 'production' ? '/rzz/' : '/',
+    plugins: [
+        react(),
+        {
+            name: "prerenderer",
+            enforce: "post",
+            apply: "build",
+            configResolved(config) {
+                // Prerenderer 配置
+                return {
+                    staticDir: config.build.outDir,
+                    routes: ["/", "/Quote", "/Markdown"],
+                    renderer: "@prerenderer/renderer-puppeteer",
+                    rendererOptions: {
+                        renderAfterDocumentEvent: "render-event",
+                        headless: true,
+                    },
+                };
+            },
+        },
+    ],
+    base: mode === "production" ? "/rzz/" : "/",
+    optimizeDeps: {
+        include: ["core-js/stable", "regenerator-runtime/runtime"],
+    },
 }));
